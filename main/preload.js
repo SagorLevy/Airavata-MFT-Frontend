1632845f81112fd527aca8b854ad34f7ffaaa487
ganning127 <ganningxu@gmail.com>
2024-03-29 11:07:18 -0400

    first commit

---
+++ main/preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+    on: (channel, callback) =>
+    {
+        ipcRenderer.on(channel, callback);
+    },
+    send: (channel, args) =>
+    {
+        ipcRenderer.send(channel, args);
+    }
+});
